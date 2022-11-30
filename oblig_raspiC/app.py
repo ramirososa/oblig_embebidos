528187ae24a210edf2ce6189d6e72603c9fe3453
ramirososa <53145455+ramirososa@users.noreply.github.com>
2022-11-29 22:14:21 -0300

    pruebA

--- oblig_raspiC/app.py
+++ oblig_raspiC/app.py
@@ -114,3 +114,15 @@ def cortarlinea(l):
     i++
     arr[2]=l[aux:i]
     return a
+
+
+import os
+
+with open('filename.txt', 'rb') as f:
+    try:  # catch OSError in case of a one line file
+        f.seek(-2, os.SEEK_END)
+        while f.read(1) != b'\n':
+            f.seek(-2, os.SEEK_CUR)
+    except OSError:
+        f.seek(0)
+    last_line = f.readline().decode()
